8df69756149e052487b627d7b5a8f8e7271cc992
Adesh Salunke <adeshsalunke2@gmail.com>
2024-09-28 13:39:47 +0530

    added debouncing in search page using lodash library

--- app/datatypes/serachResult.model.ts
+++ app/datatypes/serachResult.model.ts
@@ -3,14 +3,21 @@ export type SearchResult = {
     branded : SingleResult[]
 }
 export type SingleResult = {
-    tag_name : string,
-    serving_unit : string,
-    food_name : string,
-    tag_id : string,
-    serving_qty : string,
-    common_type : string,
-    photo : SearchImage,
-    locale : string
+    tag_name ?: string,
+    serving_unit ?: string,
+    food_name ?: string,
+    tag_id ?: string,
+    serving_qty ?: string,
+    common_type ?: string,
+    photo ?: SearchImage,
+    locale ?: string,
+    nix_brand_id ?: string,
+    brand_name_item_name ?: string,
+    nix_item_id ?: string,
+    brand_type ?: string,
+    brand_name ?: string,
+    nf_calories ?: string,
+    region ?: string,
 }
 type SearchImage = {
     thumb : string
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,35 +1,44 @@
 'use client'
 import { Autocomplete, Box, Card, CardContent, CardMedia, TextField, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { fetcher } from '../utility/fetcher';
 import { SearchResult, SingleResult } from '../datatypes/serachResult.model';
 import homePageStar from '../../public/homepagestar.png'
 import Image from 'next/image';
-const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 }]
-
+import debounce from 'lodash/debounce';
 
 export default function page() {
-    const [searchParameter, setSearchParameter] = useState(undefined);
+    const [searchParameter, setSearchParameter] = useState<string>('');
     const [serachResultBranded, setSearchResultBranded] = useState<SingleResult[]>([]);
     const [serachResultCommon , setSerachResultCommon] = useState<SingleResult[]>([]);
-    const getData = async () => {
-    const result:SearchResult = await fetcher(`/v2/search/instant/?query=${searchParameter}`,{method: "GET" , cache: 'no-cache'});
-    if(result && result.branded && result.common) {
-        setSearchResultBranded(result.branded);
-        setSerachResultCommon(result.common);
-    } 
+    // const getData = async () => {
+    // const result:SearchResult = await fetcher(`/v2/search/instant/?query=${searchParameter}`,{method: "GET" , cache: 'no-cache'});
+    // if(result && result.branded && result.common) {
+    //     setSearchResultBranded(result.branded);
+    //     setSerachResultCommon(result.common);
+    // } 
+    // }
+    // useEffect(()=>{
+    //     if(searchParameter.length>1) {
+    //         debouncedSearch();
+    //     }
+    // }, [searchParameter])
+    const debouncedSearch = useCallback(
+        debounce( async (value:string) => {
+            const result:SearchResult = await fetcher(`/v2/search/instant/?query=${value}`,{method: "GET" , cache: 'no-cache'});
+            if(result && result.branded && result.common) {
+                setSearchResultBranded(result.branded);
+                setSerachResultCommon(result.common);
+            } 
+        }, 500), // 500ms delay
+        []
+      );
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const value= e.target.value;
+        setSearchParameter(value);
+        debouncedSearch(value);
     }
-    useEffect(()=>{
-        if(searchParameter) {
-            getData();
-        }
-    }, [searchParameter])
   return (
     <>
     <div className="w-full flex justify-center">
@@ -38,10 +47,13 @@ export default function page() {
         id="free-solo-demo"
         freeSolo
         options={serachResultBranded.map((option) => option.food_name)}
-        renderInput={(params) => <TextField {...params} label="Search your favourite food..." value={searchParameter} onChange={e=> setSearchParameter(e.target.value)}/>}
-        // onChange={getData}
-      />
+        renderInput={(params) => <TextField {...params} label="Search your favourite food..." value={searchParameter} onChange={(e: React.ChangeEvent<HTMLInputElement>)=> handleChange(e)}/>}
+        />
     </div>
+    </div>
+    <div className="w-full flex mt-4 px-10">
+        <p>Filter by category</p>
+        <button className='bg-transparent px-4 py-2 rounded-3xl border'>All</button>
     </div>
     <div className="w-full flex">
         <div className='flex-1 p-4'>
@@ -54,20 +66,20 @@ export default function page() {
                     <div className="shadow-md rounded-lg p-4 flex h-full">
                         <div className="w-3/5 p-2 h-full">
                             {
-                                result.food_name.length > 12 ? 
-                                <h2 className="text-lg font-bold">{result.food_name.split(" ").slice(0,2).join(' ')}...</h2>
+                                result.food_name && result.food_name.length > 12 ? 
+                                <h2 className="text-lg font-bold">{result.food_name.split(" ").slice(0,1).join(' ')}...</h2>
                                 :<h2 className="text-lg font-bold">{result.food_name}</h2>
                             }
                             <p>{`Serving qty : ${result.serving_qty}`}</p>
                             {
-                                result.serving_unit.length > 10 ?
+                                result.serving_unit && result.serving_unit.length > 10 ?
                                 <p>{`Serving unit : ${result.serving_unit.slice(0, 10)}...`}</p>
                                 : <p>{`Serving unit : ${result.serving_unit}`}</p>
                             }
                             
                         </div>
                         <div className="w-2/5 p-2 h-full">
-                            <img src={`${result.photo.thumb}`} alt='' className='w-full h-full object-fill'
+                            <img src={`${result.photo && result.photo.thumb}`} alt='' className='w-full h-full object-fill'
                             />
                         </div>
                     </div> 
@@ -86,19 +98,19 @@ export default function page() {
                                 <div className="shadow-md rounded-lg p-4 flex h-full">
                                     <div className="w-3/5 p-2 h-full">
                                         {
-                                            result.food_name.length > 12 ? 
-                                            <h2 className="text-lg font-bold">{result.food_name.split(" ").slice(0,2).join(' ')}...</h2>
+                                            result.food_name && result.food_name.length > 12 ? 
+                                            <h2 className="text-lg font-bold">{result.food_name.split(" ").slice(0,1).join(' ')}...</h2>
                                             :<h2 className="text-lg font-bold">{result.food_name}</h2>
                                         }
                                         <p>{`Serving qty : ${result.serving_qty}`}</p>
                                         {
-                                            result.serving_unit.length > 10 ?
+                                            result.serving_unit && result.serving_unit.length > 10 ?
                                             <p>{`Serving unit : ${result.serving_unit.slice(0, 10)}...`}</p>
                                             : <p>{`Serving unit : ${result.serving_unit}`}</p>
                                         }
                                     </div>
                                     <div className="w-2/5 p-2 h-full">
-                                        <img src={`${result.photo.thumb}`} alt='' className='w-full h-full object-fill'
+                                        <img src={`${result.photo && result.photo.thumb}`} alt='' className='w-full h-full object-fill'
                                         />
                                     </div>
                                 </div> 
